c5adde929095c3db95d08a4dc929fdc491f581b0
MITSUDA Tetsuo <N>
2014-12-23 12:08:40 +0900

    locale復帰

--- manual/conf.py
+++ manual/conf.py
@@ -280,3 +280,5 @@ texinfo_documents = [
 # How to display URL addresses: 'footnote', 'no', or 'inline'.
 #texinfo_show_urls = 'footnote'
 
+locale_dirs = ['../locale/']   # path is example but recommended.
+gettext_compact = False     # optional.
